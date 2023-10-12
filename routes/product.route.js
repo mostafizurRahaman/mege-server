7444493aeece19a3bf0f8d400984a12ceb5ce4fc
mostafizurRahaman <fahim654326@gmail.com>
2023-10-12 21:17:45 +0600

    create and update product apies

---
+++ routes/product.route.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const productController = require("../controllers/product.controller");
+
+const router = express.Router();
+
+router
+   .route("/")
+   .get(productController.getAllProducts)
+   .post(productController.createProduct);
+
+router
+   .route("/:id")
+   .get(productController.getProductById)
+   .delete(productController.deleteProductById);
+
+module.exports = router;
