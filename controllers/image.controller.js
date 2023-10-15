c69871296d71f99678071ce334c58e4c6cc9827c
opuopu <nurmdopu428@gmail.com>
2023-10-15 14:53:07 +0600

    create product image multiple upload api

--- controllers/image.controller.js
+++ controllers/image.controller.js
@@ -18,3 +18,29 @@ exports.fileUpload = async (req, res, next) => {
       next(err);
    }
 };
+
+exports.multipleUpload = async (req, res, next) => {
+   try {
+      const urls = [];
+      const files = req.files;
+      console.log(files);
+      const uploadFile = async (path) => {
+         const { secure_url } = await cloudinary.uploader.upload(path);
+         return secure_url;
+      };
+
+      for (let file of files) {
+         const url = await uploadFile(file.path);
+         urls.push(url);
+         const removeResult = await promisify(fs.unlink)(file.path);
+      }
+
+      res.status(200).send({
+         status: "success",
+         message: "image uploaded successfully",
+         data: urls,
+      });
+   } catch (err) {
+      next(err);
+   }
+};
--- routes/imageupload.route.js
+++ routes/imageupload.route.js
@@ -3,6 +3,9 @@ const { uploader } = require("../middlewares/uploadImage");
 const router = express.Router();
 const imageController = require("../controllers/image.controller.js");
 
+router
+   .route("/multiple")
+   .post(uploader.array("images"), imageController.multipleUpload);
 router.route("/").post(uploader.single("image"), imageController.fileUpload);
 
 module.exports = router;
